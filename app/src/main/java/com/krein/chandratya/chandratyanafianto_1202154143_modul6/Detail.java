ec89bf19c2898b98bb055006144bb79c54124322
Chandratya <chandratya.n@gmail.com>
2018-04-01 21:25:35 +0700

    first push gan
    push

---
+++ app/src/main/java/com/krein/chandratya/chandratyanafianto_1202154143_modul6/Detail.java
@@ -0,0 +1,13 @@
+package com.krein.chandratya.chandratyanafianto_1202154143_modul6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
